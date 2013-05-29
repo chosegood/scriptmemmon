dcc65cb7123a907c71764ec9513c6da05ebe74c4
Chris Hosegood <chris.hosegood@corelogic.co.uk>
2013-05-29 10:50:26 +0100

    Now takes pid as param and outputs commit, working set and private working set

--- scriptmemmon.ps1
+++ scriptmemmon.ps1
@@ -1,3 +1,5 @@
+Param([int]$process_identifier)
+
 function GetProcessInfoById {
     param
     ([int]$processId)
@@ -5,16 +7,9 @@ function GetProcessInfoById {
     Get-WmiObject -class Win32_PerfFormattedData_PerfProc_Process | where{$_.idprocess -eq $processId} | select `
     @{Name="Process Id"; Expression = {$_.idprocess}},`
     @{Name="Counter Name"; Expression = {$_.name}},`
-    @{Name="Private Working Set"; Expression = {$_.workingSetPrivate / 1kb}}        
-}
-
-function GetProcessInfoByName
-{
-    param
-    ([string]$processName)
-
-    Get-WmiObject -class Win32_PerfFormattedData_PerfProc_Process | where{$_.name -like $processName+"*"} | select `
-    @{Name="Process Id"; Expression = {$_.idprocess}},`
-    @{Name="Counter Name"; Expression = {$_.name}},`
+    @{Name="Commit"; Expression = {$_.pageFileBytes / 1kb}},`
+    @{Name="Working Set"; Expression = {$_.workingSet / 1kb}},`
     @{Name="Private Working Set"; Expression = {$_.workingSetPrivate / 1kb}}
 }
+
+GetProcessInfoById $process_identifier
